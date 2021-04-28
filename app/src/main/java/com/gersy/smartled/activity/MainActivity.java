6d60086d6a4c0f51264080fe64572fe759930a04
gersy <gaogersy@163.com>
2021-04-28 23:12:47 +0800

    no message

--- app/src/main/java/com/gersy/smartled/activity/MainActivity.java
+++ app/src/main/java/com/gersy/smartled/activity/MainActivity.java
@@ -1,5 +1,11 @@
 package com.gersy.smartled.activity;
 
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatSeekBar;
+
 import com.gersy.smartled.R;
 import com.gersy.smartled.base.BaseActivity;
 import com.gersy.smartled.okhttp.HttpCallback;
@@ -41,9 +47,53 @@ public class MainActivity extends BaseActivity {
 //        colorPickerView.attachAlphaSlider(alphaSlideBar);
 
         // attach brightnessSlideBar
-        final BrightnessSlideBar brightnessSlideBar = findViewById(R.id.brightnessSlide);
-        colorPickerView.attachBrightnessSlider(brightnessSlideBar);
-        colorPickerView.setLifecycleOwner(this);
+//        final BrightnessSlideBar brightnessSlideBar = findViewById(R.id.brightnessSlide);
+//        colorPickerView.attachBrightnessSlider(brightnessSlideBar);
+//        colorPickerView.setLifecycleOwner(this);
+
+        AppCompatSeekBar brightnessSeekBar = findViewById(R.id.sb_brightness);
+        AppCompatSeekBar speedSeekBar = findViewById(R.id.sb_speed);
+        TextView tvFlow = findViewById(R.id.tv_flow);
+
+        brightnessSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setBrightness(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        speedSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setSpeed(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        tvFlow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startFlow();
+            }
+        });
     }
 
     @Override
@@ -54,6 +104,48 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initListener() {
 
+    }
+
+    private void startFlow(){
+        OkhttpRequest.get("http://192.168.4.1/flow").async(new HttpCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Logger.e(result);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void setBrightness(int brightness){
+        OkhttpRequest.get("http://192.168.4.1/setBrightness?brightness="+brightness).async(new HttpCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Logger.e(result);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void setSpeed(int speed){
+        OkhttpRequest.get("http://192.168.4.1/setSpeed?speed="+speed).async(new HttpCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Logger.e(result);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     private void setColor(ColorEnvelope colorEnvelope){
